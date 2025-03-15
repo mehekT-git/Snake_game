72d1bdd12112b2cdcdc843e669624f9809733d43
Mehek Trivedi <mehektrivedi28@gmail.com>
2025-03-15 18:09:39 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css'
+import GameState from './components/GameState';
+const App = () => {
+  return (
+    <div className='container'>
+      <GameState />
+    </div>
+  )
+}
+export default App;
